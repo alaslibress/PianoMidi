e9ee9bf37ced10a96707b1d7b5c8215336b14a31
Pablo <pablolaredomarfil@gmail.com>
2025-04-05 12:24:30 +0200

    Se ha creado TeclaBlanca, TeclaBlanca1, 2 y 3 y TeclaNegra pero sin programar

--- src/jd/plano/teclas/TeclaBlanca.java
+++ src/jd/plano/teclas/TeclaBlanca.java
@@ -1,10 +1,37 @@
 package jd.plano.teclas;
 
-public abstract class TeclaBlanca {
+import java.awt.*;
+
+public abstract class TeclaBlanca extends Tecla{
     public static final int ALTURA=100;
     public static final int ANCHURA=25;
 
     TeclaBlanca(int n){
-        throw new UnsupportedOperationException("No programado");
+        super(null, null, n, false, new Consola().getCapaCanvas().getGraphics());
+    }
+
+    @Override
+    public Color getColorNoPulsado() {
+        return Color.white;
+    }
+
+    @Override
+    public int getAnchura() {
+        return TeclaBlanca.ANCHURA;
+    }
+
+    @Override
+    public int getAltura() {
+        return TeclaBlanca.ALTURA;
+    }
+
+    @Override
+    protected int[] getVerticesY() {
+        return new int[0];
+    }
+
+    @Override
+    protected int[] getVerticesX() {
+        return new int[0];
     }
 }
--- src/jd/plano/teclas/TeclaBlanca1.java
+++ src/jd/plano/teclas/TeclaBlanca1.java
@@ -1,7 +1,39 @@
 package jd.plano.teclas;
 
-public class TeclaBlanca1 {
+import java.awt.*;
+
+public class TeclaBlanca1 extends TeclaBlanca{
     TeclaBlanca1(int n){
         throw new UnsupportedOperationException("No programado");
+    }
+
+    @Override
+    protected int[] getVerticesX() {
+        return new int[0];
+    }
+
+    @Override
+    protected int[] getVerticesY() {
+        return new int[0];
+    }
+
+    @Override
+    public Color getColorNoPulsado() {
+        return null;
+    }
+
+    @Override
+    public void setGtaphics(Graphics g) {
+
+    }
+
+    @Override
+    public int getAnchura() {
+        return 0;
+    }
+
+    @Override
+    public int getAltura() {
+        return 0;
     }
 }
